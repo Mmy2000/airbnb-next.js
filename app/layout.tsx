051d3f4f0f4784aae38aee3921b4ae4aa63f9559
Mmy2000 <yahianada005@gmail.com>
2024-09-22 18:20:56 +0300

    upload the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modal/Modal";
 
 
 const geistSans = localFont({
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <Navbar />
         <div className="pt-32">{children}</div>
+        <Modal/>
       </body>
     </html>
   );
