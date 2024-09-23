a8c17fc9d0c79aa83c545d4e6ccffdbd9b9aa29d
Mmy2000 <yahianada005@gmail.com>
2024-09-24 00:54:35 +0300

    starting with logout

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -23,3 +23,8 @@ export async function handleLogin(userId: string, accessToken: string, refreshTo
         path: '/'
     });
 }
+export async function resetAuthCookies() {
+    cookies().set('session_userid', '');
+    cookies().set('session_access_token', '');
+    cookies().set('session_refresh_token', '');
+}
