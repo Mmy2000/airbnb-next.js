bd71608966a59d45b5d73be2dcb6c22546299a5a
Mmy2000 <yahianada005@gmail.com>
2024-09-23 21:07:40 +0300

    display properties from backend

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,15 +1,52 @@
+"use client";
+
 import PropertyListItem from "./PropertyListItem";
+import { useEffect, useState } from "react";
 
+export type PropertyType = {
+  id: string;
+  title: string;
+  image_url: string;
+  price_per_night: number;
+  is_favorite: boolean;
+};
 
-const PropertyList = ()=>{
-    return(
-        <>
-            <PropertyListItem/>
-            <PropertyListItem/>
-            <PropertyListItem/>
-            <PropertyListItem/>
-        </>
-    )
-}
+const PropertyList = () => {
+
+    const [properties, setProperties] = useState<PropertyType[]>([]);
+
+    const getProperties = async ()=>{
+        let url = "http://localhost:8000/api/properties/";
+        await fetch(url,{
+            method:"GET"
+        })
+        .then(response => response.json())
+        .then((json)=>{
+            console.log('json',json);
+            setProperties(json.data)
+            
+        })
+        .catch((error)=>{
+            console.log(error);
+            
+        })
+    }
+
+    useEffect(() => {
+      getProperties();
+    }, []);
+  return (
+    <>
+      {properties.map((property) => {
+        return (
+          <PropertyListItem
+            key={property.id}
+            property={property}
+          />
+        );
+      })}
+    </>
+  );
+};
 
 export default PropertyList;
