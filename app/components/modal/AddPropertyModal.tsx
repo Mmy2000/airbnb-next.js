bd8f17e497c23c2831c7de93f49200740b9d3487
Mmy2000 <yahianada005@gmail.com>
2024-09-24 04:59:07 +0300

    starting with add property modal part 3

--- app/components/modal/AddPropertyModal.tsx
+++ app/components/modal/AddPropertyModal.tsx
@@ -6,11 +6,45 @@ import Modal from "./Modal";
 import useAddPropertyModal from "@/app/hooks/useAddPropertyModal";
 import apiService from "@/app/services/apiService";
 import { useRouter } from "next/navigation";
+import CustomButton from "../forms/CustomButton";
+import Categories from "../addproperty/Categories";
 
 const AddPropertyModal = () => {
+    const [currentStep, setCurrentStep] = useState(1);
+    const [dataCategory, setDataCategory] = useState("");
     const addPropertyModal = useAddPropertyModal();
     const router = useRouter();
-    const content = (<p>add property</p>)
+
+    const setCategory = (category: string) => {
+      setDataCategory(category);
+    };
+    
+    const content = (
+      <>
+        {currentStep == 1 ? (
+          <>
+            <h2 className="mb-6 text-2xl">Choose category</h2>
+            <Categories
+              dataCategory={dataCategory}
+              setCategory={(category) => setCategory(category)}
+            />
+            <CustomButton label="Next" onClick={() => setCurrentStep(2)} />
+          </>
+        ) : currentStep == 2 ? (
+          <>
+            <h2 className="mb-6 text-2xl">Discripe Your Place</h2>
+            <CustomButton
+              label="Previous"
+              className="mb-2 bg-black hover:bg-gray-800"
+              onClick={() => setCurrentStep(1)}
+            />
+            <CustomButton label="Next" onClick={() => setCurrentStep(3)} />
+          </>
+        ) : (
+          <> step 3</>
+        )}
+      </>
+    );
     return (
       <>
         <Modal
