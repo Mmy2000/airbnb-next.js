4ed1bb5220ae7e2a47d190f0119b06484c19a846
Mmy2000 <yahianada005@gmail.com>
2024-09-24 21:33:37 +0300

    starting with reservations

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -1,4 +1,15 @@
+"use client";
 
+import useLoginModal from "@/app/hooks/useLoginModal";
+import apiService from "@/app/services/apiService";
+import { useState, useEffect } from "react";
+import { Range } from "react-date-range";
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: "selection",
+};
 
 export type Property = {
   id: string;
@@ -7,10 +18,24 @@ export type Property = {
 };
 
 interface ReservationSidebarProps {
+  userId: string | null;
   property: Property;
 }
 
-const ReservationSidebar: React.FC<ReservationSidebarProps> = ({property}) => {
+const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
+  property,
+  userId
+}) => {
+  const loginModal = useLoginModal();
+
+  const [fee, setFee] = useState<number>(0);
+  const [nights, setNights] = useState<number>(1);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const [dateRange, setDateRange] = useState<Range>(initialDateRange);
+  const [minDate, setMinDate] = useState<Date>(new Date());
+  const [bookedDates, setBookedDates] = useState<Date[]>([]);
+  const [guests, setGuests] = useState<string>("1");
+  
   return (
     <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
       <h2 className="mb-5 text-2xl">${property.price_per_night} per night</h2>
--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,10 +1,12 @@
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
 import Image from "next/image";
 import apiService from "@/app/services/apiService";
+import { getUserId } from "@/app/lib/actions";
 
 
 const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
   const property = await apiService.get(`/api/properties/${params.id}`);
+  const userId = await getUserId()
   return (
     <main className="max-w-[1500px] mx-auto px-6">
       <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
@@ -45,7 +47,7 @@ const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
           <p className="mt-6 text-lg">{property.description}</p>
         </div>
         <div>
-          <ReservationSidebar property={property} />
+          <ReservationSidebar userId={userId} property={property} />
         </div>
       </div>
     </main>
