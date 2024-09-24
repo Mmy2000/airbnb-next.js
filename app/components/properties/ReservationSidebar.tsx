b6a37b90815a2ee051093adac7f5781c1b73fde4
Mmy2000 <yahianada005@gmail.com>
2024-09-24 21:50:03 +0300

    starting with reservations part 2

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -4,6 +4,8 @@ import useLoginModal from "@/app/hooks/useLoginModal";
 import apiService from "@/app/services/apiService";
 import { useState, useEffect } from "react";
 import { Range } from "react-date-range";
+import { differenceInDays, eachDayOfInterval, format } from "date-fns";
+
 
 const initialDateRange = {
   startDate: new Date(),
@@ -13,7 +15,7 @@ const initialDateRange = {
 
 export type Property = {
   id: string;
-  guests?: number;
+  guests: number;
   price_per_night: number;
 };
 
@@ -35,38 +37,70 @@ const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
   const [minDate, setMinDate] = useState<Date>(new Date());
   const [bookedDates, setBookedDates] = useState<Date[]>([]);
   const [guests, setGuests] = useState<string>("1");
-  
+  const guestsRange = Array.from(
+    { length: property.guests },
+    (_, index) => index + 1
+  );
+
+  useEffect(() => {
+
+    if (dateRange.startDate && dateRange.endDate) {
+      const dayCount = differenceInDays(dateRange.endDate, dateRange.startDate);
+
+      if (dayCount && property.price_per_night) {
+        const _fee = ((dayCount * property.price_per_night) / 100) * 5;
+
+        setFee(_fee);
+        setTotalPrice(dayCount * property.price_per_night + _fee);
+        setNights(dayCount);
+      } else {
+        const _fee = (property.price_per_night / 100) * 5;
+
+        setFee(_fee);
+        setTotalPrice(property.price_per_night + _fee);
+        setNights(1);
+      }
+    }
+  }, [dateRange]);
   return (
     <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
       <h2 className="mb-5 text-2xl">${property.price_per_night} per night</h2>
       <div className="mb-6 p-3 border border-gray-400 rounded-xl">
         <label className="mb-2 block font-bold text-xs">Guests</label>
-        <select className="w-full -ml-1 text-xm">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
+        <select
+          value={guests}
+          onChange={(e) => setGuests(e.target.value)}
+          className="w-full -ml-1 text-xm"
+        >
+          {guestsRange.map((number) => (
+            <option key={number} value={number}>
+              {number}
+            </option>
+          ))}
         </select>
       </div>
       <div className="w-full mb-6 py-6 text-center text-white transition-colors bg-airbnb hover:bg-airbnb-dark rounded-xl">
         Book
       </div>
       <div className="mb-4 flex justify-between align-center">
-        <p>$200 * 4 nights</p>
+        <p>
+          ${property.price_per_night} * {nights} nights
+        </p>
 
-        <p>$800</p>
+        <p>${property.price_per_night * nights}</p>
       </div>
 
       <div className="mb-4 flex justify-between align-center">
         <p>Djangobnb fee</p>
 
-        <p>$40</p>
+        <p>${fee}</p>
       </div>
 
       <hr />
       <div className="mt-4 flex justify-between align-center font-bold">
         <p>Total</p>
 
-        <p>$1520</p>
+        <p>${totalPrice}</p>
       </div>
     </aside>
   );
