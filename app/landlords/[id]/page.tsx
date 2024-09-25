c7f7277aac875844002bb8313bcf65946e3cb0d6
Mmy2000 <yahianada005@gmail.com>
2024-09-25 03:59:31 +0300

    fixing some issues

--- app/landlords/[id]/page.tsx
+++ app/landlords/[id]/page.tsx
@@ -9,7 +9,7 @@ const LandlordDetailPage = async ({ params }: { params: { id: string } }) => {
   const landlord = await apiService.get(`/api/auth/${params.id}`);
   const userId = await getUserId();
   return (
-    <main className="max-w-[1500px] mx-auto px-6">
+    <main className="max-w-[1200px] mx-auto px-6 mb-6">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         <aside className="col-span-1 mb-4">
           <div className="flex flex-col items-center p-6 rounded-xl border border-gray-300 shadow-xl">
@@ -31,7 +31,7 @@ const LandlordDetailPage = async ({ params }: { params: { id: string } }) => {
         </aside>
         <div className="col-span-1 md:col-span-3 pl-0 md:pl-6">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <PropertyList landlord_id={params.id} />
+            <PropertyList userId={userId} landlord_id={params.id} />
           </div>
         </div>
       </div>
