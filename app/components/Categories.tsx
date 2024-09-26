cf4d6109502ab0eaaf3c119376f4c31989781dcf
Mmy2000 <yahianada005@gmail.com>
2024-09-26 05:10:41 +0300

    starting with filters part 5

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,9 +1,48 @@
+"use client";
+
 import Image from "next/image";
+import useSearchModal , {SearchQuery} from "../hooks/useSearchModal";
+import { useState } from "react";
 
 const Categories = () => {
+  const searchModal = useSearchModal();
+  const [category, setCategory] = useState("");
+
+  const _setCategory = (_category: string) => {
+    setCategory(_category);
+
+    const query: SearchQuery = {
+      country: searchModal.query.country,
+      checkIn: searchModal.query.checkIn,
+      checkOut: searchModal.query.checkOut,
+      guests: searchModal.query.guests,
+      bedrooms: searchModal.query.bedrooms,
+      bathrooms: searchModal.query.bathrooms,
+      category: _category,
+    };
+
+    searchModal.setQuery(query);
+  };
+
   return (
     <div className="pt-3 cursor-pointer pb-3 flex items-center space-x-12">
       <div
+        onClick={() => _setCategory("")}
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+          category == "" ? "border-black" : "border-white"
+        } opacity-60 hover:border-gray-200 hover:opacity-100`}
+      >
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
+
+        <span className="text-xs">All</span>
+      </div>
+      <div
+        onClick={() => _setCategory("Beach")}
         className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white
         opacity-60 hover:border-gray-200 hover:opacity-100"
       >
@@ -17,6 +56,7 @@ const Categories = () => {
         <span className="text-xs">Beach</span>
       </div>
       <div
+        onClick={() => _setCategory("Villas")}
         className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white
         opacity-60 hover:border-gray-200 hover:opacity-100"
       >
@@ -30,6 +70,7 @@ const Categories = () => {
         <span className="text-xs">Villas</span>
       </div>
       <div
+        onClick={() => _setCategory("Tiny homes")}
         className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white
         opacity-60 hover:border-gray-200 hover:opacity-100"
       >
@@ -43,6 +84,7 @@ const Categories = () => {
         <span className="text-xs">Tiny homes</span>
       </div>
       <div
+        onClick={() => _setCategory("Cabins")}
         className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white
         opacity-60 hover:border-gray-200 hover:opacity-100"
       >
