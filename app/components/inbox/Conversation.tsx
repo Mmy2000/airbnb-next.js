150ef1d48e11e39f49bbcf4ab2f7b901671ba592
Mmy2000 <yahianada005@gmail.com>
2024-09-26 03:11:53 +0300

    fixing some issues

--- app/components/inbox/Conversation.tsx
+++ app/components/inbox/Conversation.tsx
@@ -14,15 +14,22 @@ const Conversations: React.FC<ConversationProps> = ({
   const router = useRouter();
   const otherUser = conversation.users.find((user) => user.id != userId);
   return (
-    <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
-      <p className="mb-6 text-xl">{otherUser?.name}</p>
+    <div className="px-6 py-4 cursor-pointer border flex items-center space-x-4 border-gray-300 rounded-xl">
+      <img
+        src={otherUser?.avatar_url} // Ensure the image URL is available in the message data
+        alt={otherUser?.name}
+        className="w-16 h-16 rounded-full"
+      />
+      <div>
+        <p className="mb-6 text-xl">{otherUser?.name}</p>
 
-      <p
-        onClick={() => router.push(`/inbox/${conversation.id}`)}
-        className="text-airbnb-dark"
-      >
-        Go to conversation
-      </p>
+        <p
+          onClick={() => router.push(`/inbox/${conversation.id}`)}
+          className="text-airbnb-dark"
+        >
+          Go to conversation
+        </p>
+      </div>
     </div>
   );
 };
